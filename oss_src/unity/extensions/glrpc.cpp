39e758b06722954d1bbca0e8552173b28d18450c
Yucheng Low <ylow@dato.com>
2016-06-26 04:49:54 +0000

    more glrpc tweaks

--- oss_src/unity/extensions/glrpc.cpp
+++ oss_src/unity/extensions/glrpc.cpp
@@ -13,6 +13,8 @@
 #include <logger/logger.hpp>
 #include <unity/lib/toolkit_function_macros.hpp>
 #include <unity/lib/toolkit_class_macros.hpp>
+#include <parallel/mutex.hpp>
+#include <util/dense_bitset.hpp>
 using namespace graphlab;
 
 distributed_control* dc = nullptr;
@@ -45,80 +47,97 @@ int procid() {
 
 class param_server : public graphlab::toolkit_class_base {
  public:
-  param_server() { }
+  param_server() {
+   quiting = false;
+ }
+  ~param_server() {
+    synclock.lock();
+    quiting = true;
+    cvar.signal();
+    synclock.unlock();
+    thr.join();
+  }
 
   void init() {
     rmi.reset(new dc_dist_object<param_server>(*dc, this));
+    for (procid_t p = 0; p < rmi->numprocs(); ++p) {
+      if (p != rmi->procid()) allprocs_except_me.push_back(p);
+    }
+    
   }
 
   void add_gradient(size_t elemid, size_t ptr_to_data, size_t numel) {
     float* data = reinterpret_cast<float*>(ptr_to_data);
-    std::vector<float> data_copy(data, data + numel);
-    procid_t target_machine = elemid % rmi->numprocs();
-
-    std::vector<float> delta_gradient(data_copy.size());
-    if (local_contrib[elemid].size() == 0) {
-      delta_gradient = data_copy;
-    } else {
-      for (size_t i = 0;i < data_copy.size(); ++i) {
-        delta_gradient[i] = data_copy[i] - local_contrib[elemid][i];
-      }
-    }
-    local_contrib[elemid] = data_copy;
-
-    if (target_machine == rmi->procid()) {
-      accumulate_gradient(elemid, delta_gradient);
-    } else {
-      rmi->RPC_CALL(remote_call, param_server::accumulate_gradient)(target_machine, elemid, delta_gradient);
-    }
+    std::vector<float> adata(data, data + numel);
+    locks[elemid].lock();
+    all_elements[elemid][rmi->procid()] = adata;
+    locks[elemid].unlock();
+    synclock.lock();
+    bitset.set_bit(elemid);
+    cvar.signal(); 
+    synclock.unlock();
   }
 
   void sync() {
     rmi->full_barrier();
-    clear(all_elements.size());
+  }
+
+  void get_elem_to_ptr(size_t elemid, size_t ptr_to_data, size_t numel) {
+    float* data = reinterpret_cast<float*>(ptr_to_data);
+    //memcpy((void*)(data), reinterpret_cast<void*>(&(all_elements[elemid][rmi->procid()][0])), all_elements[elemid][rmi->procid()].size() * 4);
+
+    for (size_t i = 1;i < all_elements[elemid].size(); ++i) {
+      if (i == rmi->procid()) continue;
+      const auto& e = all_elements[elemid][i];
+      if (numel == e.size()) for (size_t j = 0;j < e.size(); ++j) data[j] += e[j];
+    }
   }
 
   std::vector<float> get_elem(size_t elemid) {
-    return all_elements[elemid];
+    std::vector<float> ret = all_elements[elemid][rmi->procid()];
+    for (size_t i = 0;i < all_elements[elemid].size(); ++i) {
+      if (i == rmi->procid()) continue;
+      const auto& e = all_elements[elemid][i];
+      if (ret.size() == e.size()) for (size_t j = 0;j < ret.size(); ++j) ret[j] += e[j];
+    }
+    return ret;
   }
 
   void clear(size_t numel) {
-    all_elements.clear();
     all_elements.resize(numel);
-    num_updates.resize(numel);
-    local_contrib.resize(numel);
-  }
-  void set_gradient(size_t elemid, const std::vector<float>& data) {
-    all_elements[elemid] = data;
-  }
-
-  void accumulate_gradient(size_t elemid, const std::vector<float>& data) {
-    if (elemid >= all_elements.size()) {
-      std::cerr << "BAD no such elem!" << std::endl;
-      return;
-    }
-    std::vector<float>& localelem = all_elements[elemid];
-    if (localelem.size() > 0 && localelem.size() != data.size()) {
-      std::cerr << "BAD. Size mismatch!" << std::endl;
-      return;
-    } else if (localelem.size() == 0) {
-      localelem = data;
-    } else {
-      for (size_t i = 0;i < data.size(); ++i) localelem[i] += data[i];
-    }
-
-    int n = __sync_fetch_and_add(&(num_updates[elemid]), 1);
-    // async broadcast
-    if (n % rmi->numprocs() == 0) {
-      for (size_t p = 0; p < rmi->numprocs(); ++p) {
-        if (p != rmi->procid()) rmi->RPC_CALL(remote_call, param_server::set_gradient)(p, elemid, localelem);
+    for (size_t i = 0;i < numel; ++i) all_elements[i].resize(rmi->numprocs());
+    locks.resize(numel);
+    bitset.resize(numel); bitset.clear();
+    thr.launch([&]() {
+      synclock.lock();
+      while(!quiting) {
+        while(!quiting && bitset.popcount() == 0) {
+          cvar.wait(synclock);
+        }
+        if (quiting) break;
+        dense_bitset bscopy = bitset;
+        synclock.unlock();
+        for (size_t elemid : bscopy) {
+          bitset.clear_bit(elemid);
+          rmi->RPC_CALL(broadcast_call, param_server::set_gradient_remote)
+               (allprocs_except_me.begin(), allprocs_except_me.end(), rmi->procid(), elemid, all_elements[elemid][rmi->procid()]);
+        }
+        synclock.lock();
       }
-    }
+      synclock.unlock();
+    });
+  }
+
+  void set_gradient_remote(procid_t source, size_t elemid, const std::vector<float>& data) {
+    locks[elemid].lock();
+    all_elements[elemid][source] = data;
+    locks[elemid].unlock();
   }
 
   BEGIN_CLASS_MEMBER_REGISTRATION("param_server")
   REGISTER_CLASS_MEMBER_FUNCTION(param_server::clear, "numelem")
   REGISTER_CLASS_MEMBER_FUNCTION(param_server::get_elem, "elemid")
+  REGISTER_CLASS_MEMBER_FUNCTION(param_server::get_elem_to_ptr, "elemid", "ptr", "len")
   REGISTER_CLASS_MEMBER_FUNCTION(param_server::sync)
   REGISTER_CLASS_MEMBER_FUNCTION(param_server::add_gradient, "elemid", "ptr", "len")
   REGISTER_CLASS_MEMBER_FUNCTION(param_server::init)
@@ -126,9 +145,15 @@ class param_server : public graphlab::toolkit_class_base {
 
  private:
    std::unique_ptr<dc_dist_object<param_server>> rmi;
-   std::vector<std::vector<float>> all_elements;
-   std::vector<int> num_updates;
-   std::vector<std::vector<float>> local_contrib;
+   std::vector<std::vector<std::vector<float>>> all_elements;
+   std::vector<mutex> locks;
+   std::vector<procid_t> allprocs_except_me;
+   thread thr;
+   dense_bitset bitset;
+   mutex synclock;
+   conditional cvar;
+   bool quiting;
+   
 };
 
 BEGIN_FUNCTION_REGISTRATION
